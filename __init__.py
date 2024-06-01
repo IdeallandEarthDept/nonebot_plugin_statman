7d5fde365f532108ad7a784f3e7ff0101ddf3239
TaoismDeepLake <pymess@icloud.com>
2024-06-01 14:34:40 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -65,9 +65,10 @@ def unzip_file(zip_path, extract_to):
         zip_ref.extractall(extract_to)
     print(f"解压完成到 {extract_to}")
 
-readFile = on_notice(priority=1, block=False)
+readFile = on_notice(priority=3, block=False)
 @readFile.handle()
-async def handle_upload(bot: Bot, event: Event, state: T_State):
+async def handle_upload(bot: Bot, event: Event):
+    print("On Notice:"+str(event)+"\n")
     if event.get_event_name() == "notice.group_upload":
         newFile = event.file
         if newFile.size <= 3000000:
@@ -81,6 +82,8 @@ async def handle_upload(bot: Bot, event: Event, state: T_State):
                     if newFile.name.endswith(".zip"):
                         unzip_file(filepath, basepath)
                         check_files(basepath)
+    else:
+        print(event.get_event_description())
 
 def check_files(directory):
     """List files in directory"""
@@ -89,72 +92,6 @@ def check_files(directory):
             print(f"Found file: {file}")
         for dir in dirs:
             print(f"Found directory: {dir}")
-
-# async def download_file(url, filename):
-#     print(f'正在下载 {url} 到 {filename}')
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get(url) as response:
-#             if response.status == 200:
-#                 print(f'HTTP 状态码：{response.status}')
-#                 with open(filename, 'wb') as f:
-#                     while True:
-#                         chunk = await response.content.read(1024)
-#                         if not chunk:
-#                             break
-#                         f.write(chunk)
-#                 print(f'文件已下载并保存为 {filename}')
-#                 return None
-#             else:
-#                 print(f'下载失败，HTTP 状态码：{response.status}')
-#                 return response.status
-
-# def unzip_file(zip_path, extract_to):
-#     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-#         zip_ref.extractall(extract_to)
-
-example = on_command("统计器测试", rule=to_me(), priority=1, block=False)
-@example.handle()
-async def handle_function(args: Message = CommandArg()):
-    await example.finish("测试成功，机器人在线")
-
-
-# readFile = on_notice(priority=1, block=False)
-# @readFile.handle()
-# async def handle_function(bot: Bot, event: Event, state: T_State):
-#     print("On Notice:"+str(event)+"\n")
-#     if event.get_event_name() == "notice.group_upload":
-#         print("Group Upload\n")
-#         newFile = event.file
-#         if newFile.size <= 3000000:
-#             print(newFile.url)
-#             print(f"{newFile.name} is of {newFile.size} bytes, url is {newFile.url} \n")
-
-#             basepath = f".\\data\\{__plugin_meta__.name}"
-#             path =  f"{basepath}\\{newFile.name}"
-
-#             if not os.path.exists(basepath):
-#                 os.makedirs(basepath)
-#             elif os.path.exists(path):
-#                 print(f"[DISCARDED][DUPLICATE] {path} already exists\n")
-#                 pass
-#             else:
-#                 os.makedirs(path)
-#                 print(f"[CREATED] {path}\n")
-
-#             #download the file with the given url by https protocol
-#             if download_file(newFile.url, path) == None:
-#                 print(f"[SAVED] {newFile.name}\n")
-#                 #if newFile.name ends with .zip or .rar, extract it
-#                 if newFile.name.endswith(".zip"):
-#                     print(f"[EXTRACTING] {newFile.name}\n")
-#                     unzip_file(path, path)
-#                     pass
-#         else:
-#             print(f"[DISCARDED][SIZE_TOO_BIG] {newFile.name} is of {newFile.size} bytes, url is {newFile.url} \n")
-            
-#     print(event.get_event_description()+"\n")
-#     # print(event.get_message()+"\n")
-#     pass
 
 readMsg = on_message(priority=100, block=False)
 @readMsg.handle()
